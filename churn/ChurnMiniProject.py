848ba8bb8411404f846824d4d6cf5d49f3330677
Mustafa Emre Geçim <mustafaemregecim7@gmail.com>
2025-12-21 22:48:09 +0300

    Update

--- churn/ChurnMiniProject.py
+++ churn/ChurnMiniProject.py
@@ -12,7 +12,7 @@ dfChurn.describe()
 dfChurn.shape
 
 
-
+# Null değerleri yaklaşık float değerle doldurup type dönüşümü yapıldı.
 dfChurn["TotalCharges"].isnull().sum()
 dfChurn["TotalCharges"] = pd.to_numeric(dfChurn["TotalCharges"], errors='coerce')
 dfChurn["TotalCharges"] = dfChurn["TotalCharges"].fillna(2700.0)
@@ -20,6 +20,7 @@ dfChurn["TotalCharges"] = dfChurn["TotalCharges"].astype(float)
 dfChurn.info()
 dfChurn.value_counts()
 
+# Label encoder ile (1-0) şeklinde object kolonlar encode edildi.
 from sklearn.preprocessing import LabelEncoder
 label = LabelEncoder()
 dfChurn["gender_encode"] = label.fit_transform(dfChurn["gender"])
@@ -66,14 +67,16 @@ columns_to_labelencode = [
    "StreamingTV", "StreamingMovies", "PaperlessBilling", "Churn","MultipleLines"
 ]
 
+#Encode sonrası eski kolonlar drop edildi.
 dfChurn.drop(columns_to_labelencode,axis=1,inplace=True)
 dfChurn["MultipleLines"].value_counts()
 dfChurn.info()
 
-#Ordinal encoding uyguladım
+
 dfChurn["InternetService"].value_counts()
 dfChurn["Contract"].value_counts()
 
+#Ordinal encoding uygulaması hiyerarşi bulunan kolonlara uygulandı.
 from sklearn.preprocessing import OrdinalEncoder
 ordinal_internetservices =OrdinalEncoder(categories=[["No","DSL","Fiber optic"]])
 ordinal_contract = OrdinalEncoder(categories=[["Month-to-month","One year","Two year"]])
@@ -89,13 +92,14 @@ dfChurn["InternetService_encode"] = dfChurn["InternetService_encode"].astype(int
 dfChurn.info()
 
 
-#one-hot encoding uyguladım
+#one-hot encoding uygulandı.
 dfChurn["PaymentMethod"].value_counts()
 dfChurnencode = pd.get_dummies(dfChurn,columns=["PaymentMethod"])
 dfChurnencode.drop(["customerID"],axis=1,inplace=True)
 dfChurnencode.info()
 dfChurnencode.corr()["Churn_encode"].sort_values()
 
+# Model sağlığı için düşük kolerasyonlu(modele etkisi değiştirmeyecek kadar olan) sütunlar drop edildi.
 low_corr = [
     "gender_encode",
     "PhoneService_encode",
@@ -105,6 +109,7 @@ low_corr = [
 ]
 dfChurnencode.drop(columns=low_corr,axis=1,inplace=True)
 
+# Model kurulum - Performans izleme aşamaları yapıldı.
 
 X = dfChurnencode.drop(["Churn_encode"],axis=1)
 y = dfChurnencode["Churn_encode"]
@@ -135,6 +140,8 @@ print(matrix)
 print(report)
 print(accuracy)
 
+# Model pkl dosyası olarak kaydedildi.
+
 import joblib
 modelpkl = {
     "model": svc,
@@ -144,4 +151,5 @@ modelpkl = {
 }
 
 joblib.dump(modelpkl, "telco_churn_model.pkl")
+
 print("Model başarıyla paketlenip kaydedildi")
